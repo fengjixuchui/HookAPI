7987f1affa0795ccc2ee0c8bca4fa1e21edbda30
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-03-23 18:52:36 +0900

    improve injector

--- injector/injector.cpp
+++ injector/injector.cpp
@@ -9,6 +9,21 @@
 #include <cassert>
 #include "../config.h"
 
+BOOL IsWow64(HANDLE hProcess)
+{
+    typedef BOOL (WINAPI *FN_IsWow64Process)(HANDLE, LPBOOL);
+    HMODULE hKernel32 = GetModuleHandleA("kernel32");
+    FN_IsWow64Process pIsWow64Process =
+        (FN_IsWow64Process)GetProcAddress(hKernel32, "IsWow64Process");
+    if (!pIsWow64Process)
+        return FALSE;
+
+    BOOL bWow64;
+    if ((*pIsWow64Process)(hProcess, &bWow64))
+        return bWow64;
+    return FALSE;
+}
+
 BOOL DoCheckBits(HANDLE hProcess)
 {
     SYSTEM_INFO info;
@@ -19,6 +34,8 @@ BOOL DoCheckBits(HANDLE hProcess)
 #ifdef _WIN64
     case PROCESSOR_ARCHITECTURE_AMD64:
     case PROCESSOR_ARCHITECTURE_IA64:
+        if (IsWow64(hProcess))
+            return FALSE;
         return TRUE;
 #else
     case PROCESSOR_ARCHITECTURE_INTEL:
@@ -55,7 +72,7 @@ BOOL DoInjectDLL(DWORD pid, LPCWSTR pszDllFile)
 
     if (!DoCheckBits(hProcess))
     {
-        assert(0);
+        MessageBoxW(NULL, L"!DoCheckBits(hProcess)", NULL, MB_ICONERROR);
         return FALSE;
     }
 
@@ -63,7 +80,7 @@ BOOL DoInjectDLL(DWORD pid, LPCWSTR pszDllFile)
     LPVOID pParam = VirtualAllocEx(hProcess, NULL, cbParam, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
     if (!pParam)
     {
-        assert(0);
+        MessageBoxW(NULL, L"Out of memory!", NULL, MB_ICONERROR);
         return FALSE;
     }
 
@@ -73,7 +90,7 @@ BOOL DoInjectDLL(DWORD pid, LPCWSTR pszDllFile)
     FARPROC pLoadLibraryW = GetProcAddress(hKernel32, "LoadLibraryW");
     if (!pLoadLibraryW)
     {
-        assert(0);
+        MessageBoxW(NULL, L"!pLoadLibraryW", NULL, MB_ICONERROR);
         VirtualFreeEx(hProcess, pParam, cbParam, MEM_RELEASE);
         return FALSE;
     }
@@ -82,7 +99,7 @@ BOOL DoInjectDLL(DWORD pid, LPCWSTR pszDllFile)
         (LPTHREAD_START_ROUTINE)pLoadLibraryW, pParam, 0, NULL));
     if (!hThread)
     {
-        assert(0);
+        MessageBoxW(NULL, L"!CreateRemoteThread", NULL, MB_ICONERROR);
         VirtualFreeEx(hProcess, pParam, cbParam, MEM_RELEASE);
         return FALSE;
     }
